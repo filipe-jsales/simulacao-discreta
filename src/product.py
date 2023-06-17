b4ca29daa7cfc8d22161045c605e02dfb1a48d2d
Filipe Sales <eduardojs999@gmail.com>
2023-06-17 08:08:01 -0300

    feat: add product class

---
+++ src/product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self, product_id, name, price):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"Product {self.product_id}: {self.name}, Price: {self.price}"
