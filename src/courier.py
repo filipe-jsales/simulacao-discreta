8c9836c56064ea92bd3a1854ebfdd4d6f0668a69
Filipe Sales <eduardojs999@gmail.com>
2023-06-17 08:06:39 -0300

    feat: add actions to courier class

--- src/courier.py
+++ src/courier.py
@@ -1,9 +1,12 @@
-
 class Courier:
-    def __init__(self, courier_id, birth_date, sex):
+    def __init__(self, courier_id, birth_date, sex, courier_actions=None):
         self.courier_id = courier_id
         self.birth_date = birth_date
         self.sex = sex
+        self.courier_actions = courier_actions if courier_actions else []
 
     def __str__(self):
         return f"Courier {self.courier_id}: {self.sex}, born on {self.birth_date}"
+
+    def add_action(self, order_id, action, accept_time, deliver_time, waiting_time, waiting_time_minutes):
+        self.courier_actions.append((order_id, action, accept_time, deliver_time, waiting_time, waiting_time_minutes))
